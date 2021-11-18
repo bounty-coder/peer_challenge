a3519656609d807c98d7feac087e65e36261a468
Aman Asati <60935490+bounty-coder@users.noreply.github.com>
2021-11-18 23:14:32 +0530

    Add files via upload

---
+++ D1 10 Find the union and intersections of two arrays.py
@@ -0,0 +1,5 @@
+list1=[1,2,3,4,3,5,3,6,2,3,2,2,2,9,3,2,2]
+list2=[5,6,7,2,4,5,3,2,3,5,6,8,0,4,5,3,2,3,2]
+
+print("Union:", set(list1).union(set(list2)))
+print("Intersection:", set(list1).intersection(set(list2)))
