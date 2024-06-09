8a3e24945f673cd625b864f83df90d7e86a500ca
Aman Asati <60935490+bounty-coder@users.noreply.github.com>
2024-06-09 23:25:25 +0530

    Update D00 05 0,1 sorting.py

--- D00 05 0,1 sorting.py
+++ D00 05 0,1 sorting.py
@@ -1,7 +1,8 @@
 ## Given array of 0 and 1
 ## sort the array in O(n) time complexity
 
-
+#TC- O(n)
+#SC- O(n)
 def binfun(arr,n):
     ls=[]
     for i in range(n):
@@ -16,3 +17,25 @@ def binfun(arr,n):
 arr=list(map(int,input().split()))
 n=len(arr)
 print(binfun(arr,n))
+
+
+
+#TC-O(n)
+#SC-(1)
+
+def segregate0and1(arr,n):
+    zero=0
+    one=n-1
+    while zero<=one:
+        if arr[zero]==1 and arr[one]==0:
+            arr[zero],arr[one]=arr[one],arr[zero]
+            zero+=1
+            one-=1
+        elif arr[zero]==1 and arr[one]==1:
+            one-=1
+        elif arr[zero]==0 and arr[one]==0:
+            zero+=1
+        else:
+            zero+=1
+            one-=1
+    return arr
