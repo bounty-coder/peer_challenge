8d7cd94c2c1857c0b6140b0af52258e875124295
Aman Asati <60935490+bounty-coder@users.noreply.github.com>
2024-06-24 20:49:10 +0530

    Update D01 09 Find duplicate O(n).py

--- D01 09 Find duplicate O(n).py
+++ D01 09 Find duplicate O(n).py
@@ -8,6 +8,23 @@ def duplicate(ls):
         else:
             print(abs(ls[i]))
 
+#using XOR
+#How XOR works??
+# A number self XOR will be 0 and any with other will never be 0
+def duplicateXOR(ls):
+    ans=0
+    for i in range(len(ls)):
+        ans^=ls[i]
+    for i in range(len(ls)):
+        ans^=i
+    return ans
 
+# #EX:
+# FIrst loop
+# ans=1^2^3^3
+
+# second loop:
+# ans^1^2^3 => 1^2^3^1^2^3^3 => 3
+    
 ls = list(map(int,input().split()))
 print(duplicate(ls))
